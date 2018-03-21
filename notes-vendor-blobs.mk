b64699ade463628658a14b3c860e5b8a03375b66
seluce <36071735+seluce@users.noreply.github.com>
2018-03-21 16:11:15 +0100

    .
    .

---
+++ notes-vendor-blobs.mk
@@ -0,0 +1,11 @@
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/leagoo/z5/bin,system/bin)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/leagoo/z5/lib,system/lib)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/leagoo/z5/etc,system/etc)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/leagoo/z5/usr,system/usr)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/leagoo/z5/xbin,system/xbin)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/leagoo/z5/vendor,system/vendor)
