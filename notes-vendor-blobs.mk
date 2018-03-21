548b6a0436dfa43bb8ad807465aec5526c68e3cc
seluce <36071735+seluce@users.noreply.github.com>
2018-03-21 16:26:55 +0100

    .
    .

--- notes-vendor-blobs.mk
+++ notes-vendor-blobs.mk
@@ -1,11 +1,11 @@
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/leagoo/z5/bin,system/bin)
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/cubot/notes/bin,system/bin)
 
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/leagoo/z5/lib,system/lib)
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/cubot/notes/lib,system/lib)
 
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/leagoo/z5/etc,system/etc)
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/cubot/notes/etc,system/etc)
 
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/leagoo/z5/usr,system/usr)
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/cubot/notes/usr,system/usr)
 
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/leagoo/z5/xbin,system/xbin)
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/cubot/notes/xbin,system/xbin)
 
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/leagoo/z5/vendor,system/vendor)
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/cubot/notes/vendor,system/vendor)
