f53161b9ba5640b816c3edb1659d54857727b6f4
seluce <36071735+seluce@users.noreply.github.com>
2018-03-31 12:54:27 +0200

    fixed some bugs
    fixed some bugs

---
+++ NOTE_S-vendor-blobs.mk
@@ -0,0 +1,11 @@
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/CUBOT/NOTE_S/bin,system/bin)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/CUBOT/NOTE_S/lib,system/lib)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/CUBOT/NOTE_S/etc,system/etc)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/CUBOT/NOTE_S/usr,system/usr)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/CUBOT/NOTE_S/xbin,system/xbin)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/CUBOT/NOTE_S/vendor,system/vendor)
--- NOTE_S-vendor.mk
+++ NOTE_S-vendor.mk
@@ -11,6 +11,6 @@ PRODUCT_COPY_FILES := \
     packages/wallpapers/LivePicker/android.software.live_wallpaper.xml:/system/etc/permissions/android.software.live_wallpaper.xml
 
 # Pick up overlay for features that depend on non-open-source files
-DEVICE_PACKAGE_OVERLAYS := vendor/cubot/notes/overlay
+DEVICE_PACKAGE_OVERLAYS := vendor/CUBOT/NOTE_S/overlay
 
-$(call inherit-product, vendor/cubot/notes/notes-vendor-blobs.mk)
+$(call inherit-product, vendor/CUBOT/NOTE_S/NOTE_S-vendor-blobs.mk)
