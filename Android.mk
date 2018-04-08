04e42998fe940cddefa827f237937508578f9cd8
seluce <36071735+seluce@users.noreply.github.com>
2018-04-08 20:43:49 +0200

    .
    .

--- Android.mk
+++ Android.mk
@@ -23,7 +23,7 @@ endif
 include $(CLEAR_VARS)
 LOCAL_MODULE := libdpframework
 LOCAL_MODULE_OWNER := $VENDOR
-LOCAL_SRC_FILES := proprietary/lib/libdpframework.so
+LOCAL_SRC_FILES := lib/libdpframework.so
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_SUFFIX := .so
 LOCAL_MODULE_CLASS := SHARED_LIBRARIES
